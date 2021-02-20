594c61f605745270ea0d6a2aa72bb180fd974da7
MassinissaLab <msnsnlbh@gmail.com>
2021-02-20 14:51:20 +0100

    building gui functions

--- cryptofichier.py
+++ cryptofichier.py
@@ -22,7 +22,7 @@ class Ui_cryptofichier(QMainWindow):
         self.msg_3.hide()
         self.msg_4.hide()
         self.msg_5.hide()
-   
+        self.msg_6.hide()
        
     
         self.confirmer_btn.clicked.connect(self.generercles)
@@ -48,17 +48,66 @@ class Ui_cryptofichier(QMainWindow):
         self.msg_3.hide()
         self.msg_4.hide()
         self.msg_5.hide()
+        self.msg_6.hide()
 
         self.FilePath=self.chemin.text()
         self.ResultPath=self.rchemin.text()
 
-        extension=os.path.splitext(self.FilePath)
-        extr=os.path.splitext(self.ResultPath)
 
-        if(".docx" not in extension[1] or ".docx" not in extr[1]):
-            self.msg_2.show()
-        else :
-             print("encrypt")
+        ext1=os.path.splitext(self.FilePath)
+        ext2=os.path.splitext(self.ResultPath)
+        ext3=os.path.splitext(self.rsapc)
+        ext4=os.path.splitext(self.rsapr)
+
+        if self.FilePath=="":
+            self.msg_1.show()
+        elif self.ResultPath=="":
+            self.msg_5.show()
+        else:
+            if(".docx" not in ext1[1] or ".docx" not in ext2[1]) :
+                self.msg_2.show()
+            elif ".pem" not in ext3[1]:
+                self.msg_6.show()
+            else :
+                print("encrypt")
+                doc =docx.Document(FilePath)
+                ClearText =[]
+                cc=[]
+
+                for p in doc.paragraphs :
+                    ClearText.append(p.text)
+
+                for el in ClearText:
+                    
+                    cc.append(el.split(" "))
+
+                public, private =newkeys(1024)
+
+                
+                pk=importKey(self.rsapc)
+
+
+                docc = docx.Document() 
+                        
+
+
+                result=[]
+                
+                for p in cc:
+                    pf=[]
+                    for e in p :
+                        
+                        pf.extend(str(base64.b64encode(encrypt(e.encode('utf-8'),public)), encoding='utf-8')+" ")
+
+                    result.append(pf)
+
+                print(result)
+                for pp in result :
+                    para = docc.add_paragraph().add_run(pp)
+                docc.save(self.ResultPath)
+
+                self.msg_3.show()
+
 
 
     def decrypterf(self):
@@ -67,6 +116,7 @@ class Ui_cryptofichier(QMainWindow):
         self.msg_3.hide()
         self.msg_4.hide()
         self.msg_5.hide()
+        self.msg_6.hide()
 
         self.FilePath=self.chemin.text()
         self.ResultPath=self.rchemin.text()
@@ -87,7 +137,7 @@ class Ui_cryptofichier(QMainWindow):
         self.msg_3.hide()
         self.msg_4.hide()
         self.msg_5.hide()
-
+        self.msg_6.hide()
         FPath,Ftype = QFileDialog.getOpenFileName(self)
         
 
